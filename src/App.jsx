7dae12c30f341e7d5cf25c5f409edd741d5bd1d7
Akshay sharma <akshaysharma5432@gmail.com>
2024-08-30 03:07:09 -0400

    production ready

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import DutyShuffler from "./DutyShuffler";
+
+function App() {
+  return (
+    <>
+      <div>
+        <DutyShuffler />
+      </div>
+    </>
+  );
+}
+
+export default App;
